0ff6c470d276dfaafe1e370a5d33f07d54b2a344
Tyrn <a@b.com>
2023-01-18 21:52:37 +0300

    Sort directive sections alphabetically!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_todos/main_development.dart' as development;
-import 'package:flutter_todos/main_staging.dart' as staging;
 import 'package:flutter_todos/main_production.dart' as production;
+import 'package:flutter_todos/main_staging.dart' as staging;
 
 Future<void> main() {
   return development.main();
